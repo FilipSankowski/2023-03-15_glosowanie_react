6978eef1176fc634dd4dbae6de0c3ee8df012b7d
Royalo04 <filip.sankowski@gmail.com>
2023-03-20 22:21:34 +0100

    working vote form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,96 @@
+import { useState } from "react";
+
+function SelectCandidateOptions(props) {
+  const candidatesArray = props.candidatesArray;
+
+  const optionList = candidatesArray.map((option) => {
+    const optionText = `${option.id}. ${option.name} ${option.surname} - ${option.party}`;
+    return (
+      <option key={option.id} value={option.id}>
+        {optionText}
+      </option>
+    )
+  });
+
+  return optionList;
+}
+
 export default function Home() {
+  const defaultFormData = {
+    name:'',
+    surname:'',
+    candidateId:''
+  };
+  const [formData, setFormData] = useState(defaultFormData);
+  // TODO: ze serwera
+  const candidatesArray = [
+    {
+      id: '1',
+      name: 'name',
+      surname: 'surname',
+      party: 'party'
+    }
+  ]
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData(
+      {...formData, [name]:value}
+    )
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('formData: ', formData);
+  }
+  
   return (
     <>
-      <div>
-        Home
+      <div className='formContainer'>
+        
+        <div className='formItem'>
+          <label> Podaj imię:<br/>
+            <input
+              type='text'
+              name='name'
+              value={formData.name || ''}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div className='formItem'>
+          <label> Podaj nazwisko:<br/>
+            <input
+              type='text'
+              name='surname'
+              value={formData.surname || ''}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div className='formItem'>
+          <label> Wybierz kandydata:<br/>
+            <select
+              name='candidateId'
+              value={formData.candidateId || ''}
+              onChange={handleChange}
+            >
+              <option disabled hidden></option>
+              <SelectCandidateOptions candidatesArray={candidatesArray}/>
+            </select>
+            
+          </label>
+        </div>
+
+        <div className='formItem'>
+          <button onClick={handleSubmit}>Wyślij</button>
+        </div>
+
       </div>
     </>
-  )
-}
+  );
+};
