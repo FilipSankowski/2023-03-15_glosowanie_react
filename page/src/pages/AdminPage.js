6f42509f1e4b2f6be2706dfd75dc3f8ea19a587f
Royalo04 <filip.sankowski@gmail.com>
2023-04-10 13:59:56 +0200

    nazwiska wyborców wyświetlane na AdminPage

--- page/src/pages/AdminPage.js
+++ page/src/pages/AdminPage.js
@@ -4,28 +4,47 @@ const ResultContext = createContext();
 
 function ResultReview() {
   const voteResult = useContext(ResultContext);
-  let voterLists = {};
+  let voterData = {};
 
-  // Podzieli wszystkich głosujących na poszczególne partie - zapisze jako obiekt voterLists
+  // Podzieli wszystkich głosujących na poszczególne partie - zapisze jako obiekt voterData
   try {
     (voteResult.voters).map((voter) => {
       const text = `${voter.imie} ${voter.nazwisko}`;
       const name = voter.nazwa;
 
-      // voterLists jest obiektem który jako własność o nazwie partii przechowuje array nazwisk które na tę partię głosują
+      // voterData jest obiektem który jako własność o nazwie partii przechowuje array nazwisk które na tę partię głosują
       // Jeżeli wśród kluczy jest nazwa partii, to doda do arraya dane głosującego, jak nie ma to utworzy nową własność.
-      if ((Object.keys(voterLists)).includes(name)) {
-        voterLists = {...voterLists, [name]:[...voterLists[name], text]};
+      if ((Object.keys(voterData)).includes(name)) {
+        voterData = {...voterData, [name]:[...voterData[name], text]};
       } else {
-        voterLists = {...voterLists, [name]:[text]};
+        voterData = {...voterData, [name]:[text]};
       }
     })
+    //console.log(voterData);
   }
   catch (err) {
     //console.log(err);
   }
 
-  console.log(voterLists);
+  // Dane z voterData zapisze jako numerowane listy, które następnie zapisze do voterLists
+  const voterLists = [];
+  for (const partia of Object.keys(voterData)) {
+    // Wypunktowani wyborcy
+    const list = voterData[partia].map((name, index) => {
+      return (<li key={index}>{name}</li>)
+    });
+    // Dodanie nagłówka i zapisanie do voterLists
+    voterLists.push(
+      <div key={partia}>
+        <span>{partia}</span>
+        <ol>
+          {list}
+        </ol>
+      </div>
+    )
+  }
+
+  return voterLists
 }
 
 function ResultChart() {
